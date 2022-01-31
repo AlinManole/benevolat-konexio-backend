3d29b6f0feba92f6058d0f5abc17581b7dbdb0ed
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-01-31 16:24:08 +0100

    set index?js

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+require('dotenv').config()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const cors = require('cors')
+const dbUrl = process.env.DATABASE
+
+app.use(express.json())
+
+mongoose.connect(dbUrl)
+const db = mongoose.connection
+
+db.on('error', (err) => {
+  console.log(err)
+})
+
+db.once('open', () => {
+  console.log(`Connection to database established`)
+})
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: false
+}))
+
+app.use(cors({
+  origin: process.env.WEBSITE,
+  credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
