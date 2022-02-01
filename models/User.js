3f31b3d6ce91fac16b875aef4b125bda1a0a30fd
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-02-01 11:32:03 +0100

    passport done

--- models/User.js
+++ models/User.js
@@ -1,60 +1,66 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
-const UserSchema = Schema({
-  firstName : {
-    type : String,
-    required: true
-},
-lastName : {
-    type : String,
-    required: true
-},
-businessName : {
-    type : String,
-    required: true
-},
-email : {
-  type : String,
-  required: true,
-  unique: true
-},
-password : {
-  type : String,
-  require: true,
-  minlength: 8
-},
-telephone : {
-    type : String,
-    required: true
-},
-program : {
-  type: Object
-}, 
-messages: [{
-  type: Schema.Types.ObjectId,
-  ref : "Message"
-}]
-, 
-conversations: [{
-  type: Schema.Types.ObjectId,
-  ref : "Conversation" 
-}]
-,
-role: {
-  type: String
-},
-distanciel: {
-  type: Boolean
-},
-courses: [{
-  type: Schema.Types.ObjectId,
-  ref : "Cours"
-}]
-},{
-  timestamps: true
-})
+const UserSchema = Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    businessName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: 8,
+    },
+    telephone: {
+      type: String,
+      required: true,
+    },
+    program: {
+      type: Object,
+    },
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+    conversations: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Conversation",
+      },
+    ],
+    role: {
+      type: String,
+    },
+    distanciel: {
+      type: Boolean,
+    },
+    courses: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Cours",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
+const User = model("User", UserSchema);
 
-const User = model('User', UserSchema)
-
-module.exports = User
+module.exports = User;
