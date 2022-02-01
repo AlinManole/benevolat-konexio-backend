488ec84170cd444a47e48dec32ced11960af6068
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-02-01 16:22:40 +0100

    modif

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,64 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const User = require
+const User = require("../models/User");
 
-module.exports = app
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find().exec();
+
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+app.get("/:_id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).exec();
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+app.put("/:_id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: { ...req.body },
+      },
+      {
+        new: true
+      }
+    ).exec()
+
+    res.json(user)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+app.delete('/:id', async(req, res) => {
+  const { id } = req.params
+
+  try {
+    await User.findOneAndDelete({ _id: id }).exec()
+
+    res.json({ success: "user is deleted" })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err })
+  }
+})
+
+module.exports = app;
